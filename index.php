b38d20c8cf1936a0e78709320bb0952500ec494f
Lisa Baetsle <lisa_baetsle@hotmail.com>
2020-09-11 10:37:36 +0200

    Add the tips from Gerrit

--- index.php
+++ index.php
@@ -8,51 +8,58 @@ session_start();
 // define variables and set to empty values
 $emailErr = $streetErr = $streetnumberErr = $cityErr = $zipcodeErr = $productsErr = "";
 $email = $street = $streetnumber = $city = $zipcode = $products = "";
+$isFormValid = true;
 
 // Validate and check requirements form
 if (!empty($_POST)) {
   //email
   if (empty($_POST["email"])) {
     $emailErr = "Email is required";
+    $isFormValid = false;
   } else {
-    $email = $_POST["email"];
-  };
-
-  // check if e-mail address is well-formed
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $emailErr = "Invalid email format";
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format";
+      $isFormValid = false;
+    }
   }
 
   //street
   if (empty($_POST["street"])) {
     $streetErr = "street is required";
+    $isFormValid = false;
   } else {
-    $street = $_POST["street"];
+    $street = test_input($_POST["street"]);
   }
 
   //street number
   if (empty($_POST["streetnumber"])) {
     $streetnumberErr = "street number is required";
+    $isFormValid = false;
   } else {
-    $streetnumber = $_POST["streetnumber"];
+    $streetnumber = test_input($_POST["streetnumber"]);
     // check if the streetnumber only consists out of numbers
     if (!preg_match("/^[0-9]*$/", $streetnumber)) {
       $streetnumberErr = "Only numbers are allowed";
+      $isFormValid = false;
     }
   }
 
   //city
   if (empty($_POST["city"])) {
     $cityErr = "city is required";
+    $isFormValid = false;
   } else {
-    $city = $_POST["city"];
+    $city = test_input($_POST["city"]);
   }
 
   //zipcode
   if (empty($_POST["zipcode"])) {
     $zipcodeErr = "zipcode is required";
+    $isFormValid = false;
   } else {
-    $zipcode = $_POST["zipcode"];
+    $zipcode = test_input($_POST["zipcode"]);
     // check if the zipcode only consists out of numbers
     if (!preg_match("/^[0-9]*$/", $zipcode)) {
       $zipcodeErr = "Only numbers are allowed";
@@ -62,8 +69,9 @@ if (!empty($_POST)) {
   //products
   if (empty($_POST["products"])) {
     $productsErr = "Pick at least one product";
+    $isFormValid = false;
   } else {
-    $products = $_POST["products"];
+    $products = test_input($_POST["products"]);
   }
 };
 
@@ -85,15 +93,13 @@ if (isset($_SESSION["zipcode"])) {
   $zipcode = $_SESSION["zipcode"];
 };
 
-
-
 // The form is valid
 $submit = "";
 $deliveryTime = "";
 $totalMessage = "";
 $total = 0;
 
-if (!empty($_POST["email"]) && !empty($_POST["street"]) && !empty($_POST["streetnumber"]) && !empty($_POST["city"]) && !empty($_POST["zipcode"]) && !empty($_POST["products"])) {
+if ($isFormValid) {
   createSessionValues();
 
   $submit = 'Thank you! Your order is submitted. </br>';
@@ -112,8 +118,6 @@ function createSessionValues()
   $_SESSION["city"] = $_POST["city"];
   $_SESSION["zipcode"] = $_POST["zipcode"];
 };
-
-
 
 //your products with their price.
 if (isset($_GET["drinks"]) && $_GET['drinks'] == 1) {
@@ -169,6 +173,34 @@ if (isset($_COOKIE['amountOverall'])) {
 $cookie = "$totalAmount";
 setcookie("amountOverall", $cookie, time() + (86400 * 30), "/"); // 86400 = 1 day
 
+//Function to validate the input
+/* Validate Form Data With PHP
+The first thing we will do is to pass all variables through PHP's htmlspecialchars() function.
+
+When we use the htmlspecialchars() function; then if a user tries to submit the following in a text field:
+
+<script>location.href('http://www.hacked.com')</script>
+
+- this would not be executed, because it would be saved as HTML escaped code, like this:
+
+&lt;script&gt;location.href('http://www.hacked.com')&lt;/script&gt;
+
+The code is now safe to be displayed on a page or inside an e-mail.
+
+We will also do two more things when the user submits the form:
+
+Strip unnecessary characters (extra space, tab, newline) from the user input data (with the PHP trim() function)
+Remove backslashes (\) from the user input data (with the PHP stripslashes() function)
+The next step is to create a function that will do all the checking for us (which is much more convenient than writing the same code over and over again). */
+function test_input($data)
+{
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+// Debug function what is happening
 function whatIsHappening()
 {
   echo '<h2>$_GET</h2>';
